714573c0c55a1f0c27d7d2ea5c59dbc9191dbc0f
Sanyam <sanyamjha19@gmail.com>
2024-07-14 19:37:34 +0530

    "NameBlock gets back to home page"

--- components/NameBlock.js
+++ components/NameBlock.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function NameBlock({name, email}){
 	return (
 		<div id="name-2-1-container" 
 			className="flex-col justify-start items-center col-span-3 text-center py-[10px] sm:py-[32px] sm:pr-[35px]">
 			<div id="name-2" className="text-4xl m-3 font-bold sm:text-7xl">
-				{name}
+				<Link href='/'>
+					{name}
+				</Link>
 			</div>
 			<div id="id_links" className='flex justify-center my-3 items-center'>
 				<div id="mail" className="mx-4 content-center text-xs" >
