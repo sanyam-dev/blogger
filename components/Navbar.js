cf1c5e308eaa2d60b005d7413ad54a65106ba147
sanyam-dev <sanyamjha19@gmail.com>
2024-07-24 12:24:29 +0530

    - Added link to project homepage & gallery homepage

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ export default function NavBar(){
 		className="px-[35px] md:px-[15%] lg:px-[30%] flex place-content-between items-center text-center text-sm sm:text-base">
 			<button id="projects" className='border-b-2 border-neutral-800 hover:border-blue-200 '>
 				<div id="padded-button" className='m-[2px]'>
-					Projects
+				<Link href = "/projects">Projects</Link>
 				</div>
 			</button>
 			<button id="blog" className='border-b-2 border-neutral-800 hover:border-blue-200'>
@@ -16,7 +16,9 @@ export default function NavBar(){
 			</button>
 			<button id="gallery" className='border-b-2 border-neutral-800 hover:border-blue-200'>
 				<div id="padded-button" className='m-[2px]'>
-					Gallery
+					<Link href = '/gallery'>
+						Gallery
+					</Link>
 				</div>
 			</button>
 		</div>
