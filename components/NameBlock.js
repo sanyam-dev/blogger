d7f2ae6d030e3c13fb65d6c6ac62ffb09fee5c4c
sanyam-dev <sanyamjha19@gmail.com>
2024-07-24 12:22:36 +0530

    - changed the cv path to a prop passed down from the parent component - added "mailto:" functionality - added border around CV button for increased visibility - TODO: Hover effect around CV button in addition to cursor pointer change - code formatting

--- components/NameBlock.js
+++ components/NameBlock.js
@@ -1,24 +1,47 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-export default function NameBlock({name, email}){
+
+export default function NameBlock({name,path, email}){
+	
+	const cv_path = path;	
+
 	return (
-		<div id="name-2-1-container" 
-			className="flex-col justify-start items-center col-span-3 text-center py-[10px] sm:py-[32px] sm:pr-[35px]">
+		<div id="nameblock-container" 
+			className="flex-col justify-start items-center 
+									col-span-3 text-center py-[10px] sm:py-[32px] sm:pr-[35px]"
+		>
 			<div id="name-2" className="text-4xl m-3 no-underline sm:underline font-bold sm:text-7xl">
 				<Link href='/'>
 					{name}
 				</Link>
 			</div>
-			<div id="id_links" className='flex justify-center my-3 items-center'>
+			<div id="id_links" className='flex justify-center mt-3 sm:mt-7  items-center'>
 				<code id="mail" className="mx-4 content-center text-xs sm:text-sm" >
-					{email}
+					<Link href = {'mailto:' + email}>
+						{email}
+					</Link>
 				</code>
-				<div id="cv" className="content-center mx-4 text-xs sm:text-sm" > 
+				<div id="cv" 
+					className="
+						content-center mx-4 text-xs
+						px-2 py-[0.5px] border border-current
+						cursor-pointer sm:text-sm" > 
+					<Link
+						href = '/cv.pdf'
+						locale = {false}
+						target="_blank"
+						rel="noopener noreferrer"
+						width = "100%"
+					>
 					CV 
+					</Link>
 				</div>
 				<div id="twitter" className= "ml-2 flex justify-end">
-					<Link href='https://x.com/saumyamathur370'>
+					<Link 
+						href='https://x.com/saumyamathur370'
+						target = '_blank'
+					>
 						<Image src = "/twitter.svg" 
 							width = "64" 
 							height="64" 
