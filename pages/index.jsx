e459321c08bece714a074d762d7d7b77e150634a
sanyam-dev <sanyamjha19@gmail.com>
2024-07-24 12:27:42 +0530

    - removed MainPage dependency - Added Dashboard layout - TitleImage and IntroductionData passed as a function to Dashboard Layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,33 @@
 import Layout from '../components/Layout'
-import MainPage from '../components/MainPage'
+import DashBoardLayout from './layout'
+import Introduction from '@/components/Introduction'
+import Image from "next/image";
 
+function TitleImage(){
+	return(
+		<Image
+			src = "/img/user.png"
+			alt="not found image!"
+			width="40"
+			height="40"
+			className="w-[10rem] h-[10rem] rounded-[45%] sm:w-[225px] sm:h-[225px] sm:rounded-[0%]" // Change according to picture
+		></Image>
+	)
+}
+
+function IntroductionData(){
+	return(
+		<Introduction/>
+	)
+}
 
 export default function Home() {
+
+	
   return (
 		<>
 			<Layout>
-				<MainPage/>
+				<DashBoardLayout children = {IntroductionData()} titleCard={TitleImage()} />
 			</Layout>
 		</>
   )
