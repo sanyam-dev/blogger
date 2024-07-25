a68c3b80f0c7e4e10b9346ba8c41e5aa02d5cc04
sanyam-dev <sanyamjha19@gmail.com>
2024-07-25 17:40:35 +0530

    changed styling

--- pages/layout.js
+++ pages/layout.js
@@ -2,10 +2,10 @@ export default function DashBoardLayout({children, titleCard}){
   return(
     <div 
       id="home-container" 
-      className='sm:grid grid-cols-5 mt-[40px] h-[420px]'>
+      className='sm:grid grid-cols-5 sm:h-[29rem]'>
 				<div 
 					id="home-header" 
-					className='flex items-center justify-center sm:col-span-2 sm:h-[29rem] sm:shadow-inner sm:border-r-4 border-black bg-local'
+					className='flex items-center justify-center sm:col-span-2 sm:shadow-inner sm:border-r-4 border-black bg-local'
 					>
 					<div id="home" className='font-serif font-bold text-3xl border-b-2 border-black  sm:border-none sm:text-[70px]'>
 						{titleCard}
@@ -13,8 +13,8 @@ export default function DashBoardLayout({children, titleCard}){
 				</div>
 				<div 
           id="list" 
-          className='col-span-3 pl-5 pr-5 
-          sm:pl-10 sm:shadow-inner overflow-scroll'>
+          className='col-span-3 px-10
+          sm:px-20 sm:shadow-inner overflow-scroll'>
             {children}
 				</div>
 			</div>
